b2bdc33249db3d25efb877c50505c8a61f17c7d2
Marzan1810662 <hassanmarzan17@gmail.com>
2022-06-05 16:34:17 +0600

    slider added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from '../../Slider';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Header/>
+            <Slider/>
             <Footer/>
         </div>
     );
