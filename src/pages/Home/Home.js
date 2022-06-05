3941d641411f4c662799c07bc088073567ec4fd0
Marzan1810662 <hassanmarzan17@gmail.com>
2022-06-05 17:31:34 +0600

    simple steps added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Slider from '../../Slider';
 import Footer from './Footer';
 import Header from './Header';
+import SimpleSteps from './SimpleSteps';
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <Slider/>
+            <SimpleSteps/>
             <Footer/>
         </div>
     );
---
+++ src/pages/Home/SimpleSteps.js
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import mail from '../../images/icons/mail.jpg'
+import document from '../../images/icons/documents.jpg'
+import download from '../../images/icons/download.jpg'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+const SimpleSteps = () => {
+    return (
+        <div className='border-t'>
+            <div className='py-8 my-2 '>
+                <h1 className='text-5xl '>3 simple Steps</h1>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 3xl:gap-8 px-4 lg:px-28 my-6'>
+                    <div className=' p-3 flex flex-col justify-center items-center'>
+                        <img className='w-44' src={mail} alt="" />
+                        <p className='text-3xl'>Step Number 1 </p>
+                        <p>Contact Us <FontAwesomeIcon icon={faAngleRight}  className='ml-2 text-sm'  /></p>
+                    </div>
+                    <div className=' p-3  flex flex-col justify-center items-center'>
+                        <img className='w-44' src={document} alt="" />
+                        <p className='text-3xl'>Step Number 2</p>
+                        <p>tell us your <br /> requirements <FontAwesomeIcon icon={faAngleRight}  className='ml-2 text-sm'  /></p>
+                    </div>
+                    <div className='hidden'>
+
+                    </div>
+                    <div className=' p-3  flex flex-col justify-center items-center'>
+                        <img className='w-44' src={download} alt="" />
+                        <p className='text-3xl'>Step Number 3 </p>
+                        <p>Get your website <FontAwesomeIcon icon={faAngleRight}  className='ml-2 text-sm'  /></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SimpleSteps;
